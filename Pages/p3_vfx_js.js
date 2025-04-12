40835e8f1b38a1677f23b521807e1bbe20ea69f3
pinganchido <45507363+pinganchido@users.noreply.github.com>
2025-04-12 17:29:31 +0800

    Add files via upload

---
+++ Pages/p3_vfx_js.js
@@ -0,0 +1,76 @@
+/*
+  This is your site JavaScript code - you can add interactivity!
+*/
+
+// Print a message in the browser's dev tools console each time the page loads
+// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+console.log("Hello 🌎");
+
+/* 
+Make the "Click me!" button move when the visitor clicks it:
+- First add the button to the page by following the steps in the TODO 🚧
+*/
+const btn = document.querySelector("button"); // Get the button from the page
+if (btn) { // Detect clicks on the button
+  btn.onclick = function () {
+    // The 'dipped' class in style.css changes the appearance on click
+    btn.classList.toggle("dipped");
+  };
+}
+
+
+// ----- GLITCH STARTER PROJECT HELPER CODE -----
+
+// Open file when the link in the preview is clicked
+let goto = (file, line) => {
+  window.parent.postMessage(
+    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
+  );
+};
+// Get the file opening button from its class name
+const filer = document.querySelectorAll(".fileopener");
+filer.forEach((f) => {
+  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+});
+//scrolling
+window.addEventListener("scroll", function () {
+  const scrollY = window.scrollY;
+  const i1 = document.getElementById("i1").offsetTop;
+  const i2 = document.getElementById("i2").offsetTop;
+  const i3 = document.getElementById("i3").offsetTop;
+  const wrapper = document.querySelector(".wrapper");
+  
+  if (scrollY >= i3 ) {
+    document.body.style.backgroundColor = "lightcoral";
+    wrapper.style.backgroundColor = "lightcoral";
+  } else if (scrollY >= i2) {
+    document.body.style.backgroundColor = "lightblue";
+    wrapper.style.backgroundColor = "lightblue";
+  } else {
+    document.body.style.backgroundColor = "#F0FFF0"	;
+     wrapper.style.backgroundColor = "#F0FFF0";
+  }
+});
+
+// Get the section element
+const section = document.getElementById("i2");
+const i2_oriCol = "lightblue";
+// Define colors and index
+const bgColors = ["#f0f8ff", "#ffcccb", "#d1e7dd", "#fff3cd", "#dbeafe"];
+let bgIndex = 0;
+
+// Listen for key press
+/*document.addEventListener("click", () => {
+  const scrollY = window.scrollY;
+  const i2Top = document.getElementById("i2").offsetTop;
+  const i2Bottom = i2Top + document.getElementById("i2").offsetHeight;
+  const wrapper = document.querySelector(".wrapper");
+  if (scrollY >= i2Top && scrollY < i2Bottom) {
+    wrapper.style.backgroundColor = bgColors[bgIndex];
+    bgIndex = (bgIndex + 1) % bgColors.length;
+  } else {
+    wrapper.style.backgroundColor = ""; // Remove background override
+  }
+});
+
+*/
